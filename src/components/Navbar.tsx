9cc0ecf319e8b17e66eb58bb4dea3c3c5c221e1e
Monish.R <140420819+MonishRaman@users.noreply.github.com>
2024-12-19 23:30:59 +0530

    done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,9 +15,12 @@ export default function Navbar({ isDarkMode, toggleTheme }: NavbarProps) {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <Link to="/" className="text-xl font-bold text-gray-900 dark:text-white">
-              CarShowcase
-            </Link>
+          <Link
+      to="/"
+      className="text-4xl font-bold text-gray-900 dark:text-white font-audiowide"
+    >
+      CarSphere
+    </Link>
           </div>
 
           {/* Desktop Navigation */}
